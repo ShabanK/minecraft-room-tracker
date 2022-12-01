7851bf2ff0e043f31015aef7d6b434a71b27438c
ShabanK <shabankhawar99@gmail.com>
2022-12-02 04:04:40 +0500

    grid fixed

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,28 +1,58 @@
-import MapTracker from "./entities/maptracker";
+// import MapTracker from "./entities/maptracker";
+import Room from "./entities/room";
 import { useState, useEffect } from "react";
 
 export default function Box() {
-  const [map, setMap] = useState(null);
-
-  useEffect(() => {
-    setMap(new MapTracker());
-    console.log(map);
-  }, []);
+  const gridRadius = 5;
+  const [grid, setGrid] = useState(() => {
+    let arr = new Array(gridRadius * 2 + 1).fill().map(function () {
+      return new Array(gridRadius * 2 + 1).fill().map(() => {
+        return null;
+      });
+    });
+    arr[gridRadius][gridRadius] = new Room("60,60", true);
+    return arr;
+  });
 
   function addRoom(direction) {
     console.log(direction);
   }
 
   function debug() {
-    console.log(map);
+    console.log(grid);
+    console.log(grid[gridRadius][gridRadius]);
   }
 
   return (
-    <div className="grid">
-      <div className="row">
-        <div className="box"></div>
+    <>
+      <div className="grid">
+        {grid.map((rows, rowIndex) => {
+          return (
+            <div key={rowIndex} className="row">
+              {rows.map((box, boxIndex) => {
+                box && console.log(box);
+                return box ? (
+                  <div key={boxIndex} className="box">
+                    HOME {rowIndex} {boxIndex}
+                  </div>
+                ) : (
+                  <div key={boxIndex} className="box">
+                    {rowIndex},{boxIndex}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
       </div>
-    </div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <button onClick={() => addRoom("up")}>up</button>
+        <button onClick={() => addRoom("down")}>down</button>
+        <button onClick={() => addRoom("left")}>left</button>
+        <button onClick={() => addRoom("right")}>right</button>
+        <button onClick={debug}>test</button>
+      </div>
+    </>
   );
   {
     /* 
@@ -35,13 +65,7 @@ export default function Box() {
             </div>
           ))}
       </div>
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <button onClick={() => addRoom("up")}>up</button>
-        <button onClick={() => addRoom("down")}>down</button>
-        <button onClick={() => addRoom("left")}>left</button>
-        <button onClick={() => addRoom("right")}>right</button>
-        <button onClick={debug}>test</button>
-      </div> 
+      
       </>
     */
   }
