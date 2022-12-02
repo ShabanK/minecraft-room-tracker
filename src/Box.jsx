3ddb6d95b9cd20b976eb873392c7df8dc3aaa5e1
ShabanK <shabankhawar99@gmail.com>
2022-12-02 06:10:32 +0500

    room creation is a go

--- src/Box.jsx
+++ src/Box.jsx
@@ -7,18 +7,82 @@ export default function Box() {
   const [grid, setGrid] = useState(() => {
     let arr = new Array(gridRadius * 2 + 1).fill().map(function () {
       return new Array(gridRadius * 2 + 1).fill().map(() => {
-        return null;
+        return "";
       });
     });
-    arr[gridRadius][gridRadius] = new Room("60,60", true);
+    arr[gridRadius][gridRadius] = new Room(`${gridRadius}/${gridRadius}`, true);
     return arr;
+  });
+  const [currentSelection, setCurrentSelection] = useState({
+    row: gridRadius,
+    col: gridRadius,
   });
 
   function addRoom(direction) {
     console.log(direction);
+    const { row, col } = currentSelection;
+    let targetRow = null,
+      targetCol = null;
+    switch (direction) {
+      case "up":
+        if (row - 1 >= 0) {
+          if (!grid[row - 1][col]) {
+            targetRow = row - 1;
+            targetCol = col;
+          }
+        }
+        break;
+      case "down":
+        if (row + 1 <= gridRadius * 2) {
+          if (!grid[row + 1][col]) {
+            console.log("fire");
+            targetRow = row + 1;
+            targetCol = col;
+          }
+        }
+        break;
+      case "left":
+        if (col - 1 >= 0) {
+          if (!grid[row][col - 1]) {
+            // console.log(col - 1, grid[row][col - 1]);
+            targetRow = row;
+            targetCol = col - 1;
+          }
+        }
+        break;
+      case "right":
+        if (col + 1 <= gridRadius * 2) {
+          if (!grid[row][col + 1]) {
+            targetRow = row;
+            targetCol = col + 1;
+          }
+        }
+        break;
+    }
+
+    console.log({ targetRow, targetCol });
+
+    if (Number.isInteger(targetRow) && Number.isInteger(targetCol)) {
+      // update grid
+      setGrid(() => {
+        return grid.map((rows, rowIndex) => {
+          return rows.map((room, colIndex) => {
+            if (rowIndex === targetRow && colIndex === targetCol) {
+              return new Room(`${targetRow}/${targetCol}`, false);
+            } else return room;
+          });
+        });
+      });
+
+      // update selection
+      setCurrentSelection({ row: targetRow, col: targetCol });
+    }
   }
 
-  function debug() {}
+  function debug() {
+    // console.log(currentSelection);
+    console.log(grid);
+  }
 
   return (
     <>
@@ -33,9 +97,7 @@ export default function Box() {
                     HOME
                   </div>
                 ) : (
-                  <div key={boxIndex} className="box">
-                    {rowIndex},{boxIndex}
-                  </div>
+                  <div key={boxIndex} className="boxBlank"></div>
                 );
               })}
             </div>
